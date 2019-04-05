b6b4c6616e39ff4b38f2de373facc821d8b92642
Root USO User <student@stud.acs.upb.ro>
2019-04-05 13:54:38 +0300

    test

--- labtasks/ClientServerCommunication/app/src/main/java/ro/pub/cs/systems/eim/lab06/clientservercommunication/network/ClientAsyncTask.java
+++ labtasks/ClientServerCommunication/app/src/main/java/ro/pub/cs/systems/eim/lab06/clientservercommunication/network/ClientAsyncTask.java
@@ -4,12 +4,18 @@ import android.os.AsyncTask;
 import android.util.Log;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 import ro.pub.cs.systems.eim.lab06.clientservercommunication.general.Constants;
+import ro.pub.cs.systems.eim.lab06.clientservercommunication.general.Utilities;
 
 public class ClientAsyncTask extends AsyncTask<String, String, Void> {
 
     private TextView serverMessageTextView;
-
+    Socket socket = null;
     public ClientAsyncTask(TextView serverMessageTextView) {
         this.serverMessageTextView = serverMessageTextView;
     }
@@ -26,10 +32,33 @@ public class ClientAsyncTask extends AsyncTask<String, String, Void> {
             // by publishing the progress - with the publishProgress(...) method - to the UI thread
             // - close the socket to the server
 
-        } catch (Exception exception) {
-            Log.e(Constants.TAG, "An exception has occurred: " + exception.getMessage());
+            int serverPort = Integer.parseInt(params[1]);
+            socket = new Socket(params[0], 2017);
+            Log.v(Constants.TAG, "Connected to: addr:" + socket.getInetAddress() + ":port:" + socket.getLocalPort());
+            BufferedReader bufferedReader = Utilities.getReader(socket);
+            String line = bufferedReader.readLine();
+            Log.v(Constants.TAG, "received: " + line);
+            publishProgress(line);
+        } catch (UnknownHostException unknownHostException) {
+            Log.d(Constants.TAG, unknownHostException.getMessage());
             if (Constants.DEBUG) {
-                exception.printStackTrace();
+                unknownHostException.printStackTrace();
+            }
+        } catch (IOException ioException) {
+            Log.d(Constants.TAG, ioException.getMessage());
+            if (Constants.DEBUG) {
+                ioException.printStackTrace();
+            }
+        } finally {
+            try {
+                if (socket != null) {
+                    socket.close();
+                }
+            } catch (IOException ioException) {
+                Log.d(Constants.TAG, ioException.getMessage());
+                if (Constants.DEBUG) {
+                    ioException.printStackTrace();
+                }
             }
         }
         return null;
@@ -39,12 +68,14 @@ public class ClientAsyncTask extends AsyncTask<String, String, Void> {
     protected void onPreExecute() {
         // TODO exercise 6b
         // - reset the content of the serverMessageTextView
+        serverMessageTextView.setText("");
     }
 
     @Override
     protected void onProgressUpdate(String... progress) {
         // TODO exercise 6b
         // - append the content to serverMessageTextView
+        serverMessageTextView.append(progress[0] + '\n');
     }
 
     @Override
